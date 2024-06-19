d38422c5aa7b3460f942f1544bb007c40cec41fc
e-book-store <rajvardhandesai1111@gmail.com>
2024-06-19 16:57:20 +0530

    minor

--- lib/widgets/user_avatar.dart
+++ lib/widgets/user_avatar.dart
@@ -1,5 +1,3 @@
-// lib/widgets/user_avatar.dart
-
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter_blurhash/flutter_blurhash.dart';
