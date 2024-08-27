50be5fec61848e52f45849710a02f12a6593c7c8
e-book-store <rajvardhandesai1111@gmail.com>
2024-08-27 21:33:10 +0530

    improved profile page

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,8 +1,10 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:alumniconnect/screens/edit_profile_screen.dart';
-import 'package:alumniconnect/widgets/user_avatar.dart';
+
+import '../widgets/user_avatar.dart';
+import 'change_password_screen.dart';
+import 'edit_profile_screen.dart';
 
 class ProfilePage extends StatefulWidget {
   final String userName;
@@ -10,7 +12,7 @@ class ProfilePage extends StatefulWidget {
   final String? userImageUrl;
   final String? blurHash;
 
-  ProfilePage({
+  const ProfilePage({
     super.key,
     required this.userName,
     required this.userEmail,
@@ -19,10 +21,10 @@ class ProfilePage extends StatefulWidget {
   });
 
   @override
-  _ProfilePageState createState() => _ProfilePageState();
+  ProfilePageState createState() => ProfilePageState();
 }
 
-class _ProfilePageState extends State<ProfilePage> {
+class ProfilePageState extends State<ProfilePage> {
   String? _userImageUrl;
   late FirebaseAuth _auth;
 
@@ -39,63 +41,119 @@ class _ProfilePageState extends State<ProfilePage> {
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            UserAvatar(
-              key: ValueKey(_userImageUrl), // Ensure the widget rebuilds with a new key
-              imageUrl: _userImageUrl,
-              blurHash: widget.blurHash,
+            const SizedBox(height: 20),
+            CircleAvatar(
               radius: 60,
+              child: UserAvatar(
+                key: ValueKey(_userImageUrl),
+                imageUrl: _userImageUrl,
+                blurHash: widget.blurHash,
+                radius: 55,
+                iconSize: 60, // Slightly smaller radius inside the border
+              ),
             ),
             const SizedBox(height: 10),
             Text(
               widget.userName,
               style: const TextStyle(
-                fontSize: 24.0,
+                fontSize: 28.0,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            const SizedBox(height: 5),
+            const SizedBox(height: 4),
             Text(
               widget.userEmail,
               style: TextStyle(
                 fontSize: 16.0,
-                color: Colors.grey[600],
+                color: Colors.grey[700],
               ),
             ),
-            const SizedBox(height: 20),
-            ProfileOption(
-              icon: Icons.edit,
-              text: 'Edit Profile',
-              onTap: () => _navigateToEditProfile(context),
-            ),
+            const SizedBox(height: 10),
             const Divider(),
-            ProfileOption(
-              icon: Icons.support,
-              text: 'Support',
-              onTap: () {
-                // Implement support navigation or function
-              },
-            ),
-            ProfileOption(
-              icon: Icons.article,
-              text: 'Terms of Service',
-              onTap: () {
-                // Implement terms of service navigation or function
-              },
-            ),
-            ProfileOption(
-              icon: Icons.group_add,
-              text: 'Invite Friends',
-              onTap: () {
-                // Implement invite friends function
-              },
-            ),
-            ProfileOption(
-              icon: Icons.logout,
-              text: 'Sign Out',
-              onTap: () => _confirmSignOut(context),
-            ),
+            const SizedBox(height: 4),
+            _buildProfileOptions(),
+            const SizedBox(height: 16),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProfileOptions() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        _buildSectionTitle('Account'),
+        ProfileOption(
+          icon: Icons.edit_outlined,
+          text: 'Edit Profile',
+          onTap: () => _navigateToEditProfile(context),
+        ),
+        ProfileOption(
+          icon: Icons.lock_outline,
+          text: 'Change Password',
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const ChangePasswordScreen()),
+            );
+          },
+        ),
+        _buildSectionTitle('App'),
+        ProfileOption(
+          icon: Icons.group_outlined,
+          text: 'Invite Friends',
+          onTap: () {
+            // Implement invite friends navigation or function
+          },
+          subtitle: 'Invite your friends to join the app',
+        ),
+        _buildSectionTitle('About'),
+        ProfileOption(
+          icon: Icons.help_outline_rounded,
+          text: 'Help',
+          onTap: () {
+            // Implement help navigation or function
+          },
+        ),
+        ProfileOption(
+          icon: Icons.description_outlined,
+          text: 'Terms of Service',
+          onTap: () {
+            // Implement terms of service navigation or function
+          },
+        ),
+        ProfileOption(
+          icon: Icons.info_outline_rounded,
+          text: 'AlumniConnect',
+          onTap: () {
+            // Implement terms of service navigation or function
+          },
+          subtitle: "Developed by Rajvardhan Desai\nbeta.2024.x28",
+        ),
+        ProfileOption(
+          icon: Icons.logout,
+          text: 'Sign Out',
+          fontStyle: const TextStyle(color: Color(0xffee4c41)),
+          onTap: () => _confirmSignOut(context),
+          iconColor: const Color(0xffee4c41), // Custom icon color for sign out
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSectionTitle(String title) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
+      child: Text(
+        title,
+        style: TextStyle(
+          fontSize: 14.0,
+          color: Colors.grey[600],
+          fontWeight: FontWeight.bold,
         ),
       ),
     );
@@ -110,41 +168,21 @@ class _ProfilePageState extends State<ProfilePage> {
     if (result == true && widget.userImageUrl != null) {
       await _clearImageCache(widget.userImageUrl!);
       setState(() {
-        _userImageUrl = '${widget.userImageUrl}?t=${DateTime.now().millisecondsSinceEpoch}';
+        _userImageUrl = Uri.parse(widget.userImageUrl!).replace(
+            queryParameters: {
+              't': DateTime.now().millisecondsSinceEpoch.toString()
+            }).toString();
       });
     }
   }
 
   Future<void> _clearImageCache(String imageUrl) async {
     await CachedNetworkImage.evictFromCache(imageUrl);
-    await CachedNetworkImage.evictFromCache('$imageUrl?t=1'); // Clear with a temp param to ensure complete cache clear
-  }
-
-  void _confirmSignOut(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Confirm Sign Out'),
-          content: const Text('Are you sure you want to sign out?'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-                _signOut(context);
-              },
-              child: const Text('Sign Out'),
-            ),
-          ],
-        );
-      },
-    );
+    final parsedUri = Uri.parse(imageUrl);
+    if (parsedUri.queryParameters.isNotEmpty) {
+      await CachedNetworkImage.evictFromCache(
+          parsedUri.replace(queryParameters: {}).toString());
+    }
   }
 
   Future<void> _signOut(BuildContext context) async {
@@ -158,11 +196,62 @@ class _ProfilePageState extends State<ProfilePage> {
       if (context.mounted) {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
-            content: Text('Error signing out. Please try again. $e'),
+            content: Text(
+                'Error signing out: ${e.toString()}. Please try again.'),
           ),
         );
       }
     }
+  }
+
+  void _confirmSignOut(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                'Confirm Sign Out',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 16),
+              const Text('Are you sure you want to sign out?'),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('Cancel'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                      _signOut(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xffff2121),
+                      foregroundColor: Colors.white,
+                    ),
+                    child: const Text('Sign Out',
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 }
 
@@ -170,33 +259,38 @@ class ProfileOption extends StatelessWidget {
   final IconData icon;
   final String text;
   final VoidCallback onTap;
+  final String? subtitle; // Optional subtitle
+  final TextStyle? fontStyle; // Optional font style
+  final Color? iconColor; // Optional icon color
 
   const ProfileOption({
     super.key,
     required this.icon,
     required this.text,
     required this.onTap,
+    this.subtitle, // Initialize subtitle
+    this.fontStyle, // Initialize font style
+    this.iconColor, // Initialize icon color
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric(vertical: 8.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.2),
-            spreadRadius: 2,
-            blurRadius: 5,
-            offset: const Offset(0, 3),
-          ),
-        ],
-      ),
+      margin: const EdgeInsets.symmetric(vertical: 2.0),
       child: ListTile(
-        leading: Icon(icon),
-        title: Text(text),
+        leading: Icon(
+          icon,
+          color: iconColor ?? const Color(0xff575f64), // Use provided icon color or default
+          size: 28,
+        ),
+        title: Text(
+          text,
+          style: fontStyle ?? const TextStyle( // Use provided font style or default
+            fontSize: 16.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        subtitle: subtitle != null ? Text(subtitle!) : null, // Show subtitle if provided
         onTap: onTap,
       ),
     );
